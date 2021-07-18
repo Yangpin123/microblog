367a96cf21880eba54922455ffaa7c06fe470ace
Yangpin123 <1197344397@qq.com>
2021-07-18 17:21:27 +0800

    login_logout_register

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from config import Config #从config模块导入Config类
 
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 #将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app = Flask(__name__)
@@ -11,6 +13,9 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 #print('等会谁（哪个包或模块）在使用我：',__name__)
 #从app包中导入模块routes
